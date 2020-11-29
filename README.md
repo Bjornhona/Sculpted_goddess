986c5b0c49a39cd7e9afd08e57ec6ebe797a257c
Bjornhona <asaeri3@hotmail.com>
2020-11-29 22:46:04 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,65 @@
+## Project title
+A financial independence calculator
+It is a tool that calculates, based on my: current savings, investments, income, retirement accounts, and expenses, how far away I am from financial independence.
+
+## Motivation
+This is a web application that calculates how far away I am from financial freedom. It will save and invest as much of my income as possible so that I don’t need to work for money. It is also my Final Project as part of the Harvard course CS50x on edX, Introduction to Computer Science.
+
+## Build status
+Build status of continus integration i.e. travis, appveyor etc. Ex. - 
+
+[![Build Status](https://travis-ci.org/akashnimare/foco.svg?branch=master)](https://travis-ci.org/akashnimare/foco)
+[![Windows Build Status](https://ci.appveyor.com/api/projects/status/github/akashnimare/foco?branch=master&svg=true)](https://ci.appveyor.com/project/akashnimare/foco/branch/master)
+
+## Code style
+If you're using any code style like xo, standard etc. That will help others while contributing to your project. Ex. -
+
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat)](https://github.com/feross/standard)
+ 
+## Screenshots
+Include logo/demo screenshot etc.
+
+## Tech/framework used
+<b>Built with</b>
+- Visual Studio Code
+- Backend in Python with Flask
+- Frontend in Javascipt, HTML and CSS
+- Database in SQL
+
+<b>Other tools used<b>
+- Design in Adobe XD
+- Trello Board
+
+## Features
+What makes your project stand out?
+
+## Code Example
+Show what the library does as concisely as possible, developers should be able to figure out **how** your project solves their problem by looking at the code example. Make sure the API you are showing off is obvious, and that your code is short and concise.
+
+## Installation
+Provide step by step series of examples and explanations about how to get a development env running.
+
+## API Reference
+
+Depending on the size of the project, if it is small and simple enough the reference docs can be added to the README. For medium size to larger projects it is important to at least provide a link to where the API reference docs live.
+
+## Tests
+Describe and show how to run the tests with code examples.
+
+## How to use?
+If people like your project they’ll want to learn how they can use it. To do so include step by step guide to use your project.
+
+## Contribute
+
+Let people know how they can contribute into your project. A [contributing guideline](https://github.com/zulip/zulip-electron/blob/master/CONTRIBUTING.md) will be a big plus.
+
+## Credits
+Thank you very much to David J. Malan for great explanations during all of his teaching sessions and also to the rest of the team at Harvard CS50 for a very professional build up couse.
+I found good inspiration for this particular project online, great thanks to FIREcalc and cFIREsim.
+
+#### Anything else that seems useful
+
+## License
+A short snippet describing the license (MIT, Apache etc)
+
+MIT © Asa Eriksson
---
+++ app.py
@@ -0,0 +1,113 @@
+from sqlalchemy import create_engine, select
+
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+# from datetime import datetime
+
+from helpers import login_required, apology
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure SQLAlchemy Library to use SQLite database
+engine = create_engine("sqlite:///beautygifts.db", echo=True)
+
+@app.route("/")
+@login_required
+def index():
+    """Show home page"""
+
+    return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        result = engine.execute("SELECT * FROM users WHERE username = :username",
+                          username=request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if not result:
+            return apology("Invalid username", 403)
+        else:
+            for row in result:
+                if check_password_hash(row['hash'], request.form.get("password")):
+                    session["user_id"] = row["id"]
+
+                    # Redirect user to home page
+                    return redirect("/")
+                
+            return apology("invalid password", 403)
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    if request.method == "GET":
+        return render_template("register.html")
+    else:
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+
+        if not username:
+            return apology("You must provide a Username", 400)
+        if not password:
+            return apology("You must provide a Password", 400)
+        if not confirmation:
+            return apology("You must repeat your password", 400)
+        if password != confirmation:
+            return apology("Your passwords did not match", 400)
+
+        engine.execute("INSERT INTO users (username, hash) VALUES (:username, :hash)", username=username, hash=generate_password_hash(password))
+        return redirect("/login")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
